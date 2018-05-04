870e1749e167cbd05147959762022dc2d964d2fd
bello64761 <37994513+bello64761@users.noreply.github.com>
2018-05-04 00:30:50 +0000

    Update test1912.java

--- test1912.java
+++ test1912.java
@@ -5,14 +5,14 @@ public class test1912 {
   public static void main(String[] args) {  
     // TODO Auto-generated method stub  
     Scanner sc = new Scanner(System.in);  
-    System.out.print("n을 입력하세요.(n<=12) : ");  
+    System.out.print("자연수 n을 입력하세요.(n<=12) : ");  
     int input = sc.nextInt();  
     System.out.println(factorial(input)); 
  }
  
   public static int factorial(int n) {  
-    if (n <= 1) {    
-        return n;  
+    if (n == 1) {    
+        return 1;  
      }  
     else {  
         return n * factorial(n-1);  
